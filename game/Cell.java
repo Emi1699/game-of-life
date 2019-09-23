519ff14a6d3f1ea14d1fa40d57655e5bbe5218eb
Emi1699 <emisv_16@yahoo.ro>
2019-09-23 22:33:00 +0100

    created Cell class

---
+++ game/Cell.java
@@ -0,0 +1,16 @@
+package game;
+
+public class Cell {
+	private int x, y;
+	private int aliveNeighbours;
+	
+	public Cell() {
+		
+	}
+	
+	public void countAliveNeighbours() {
+		
+	}
+	
+	//LEFT TO DO: ADD HANDLER
+}

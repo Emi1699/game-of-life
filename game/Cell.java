da66eea283a458664b8a87c99b88662959d61549
Emi1699 <emisv_16@yahoo.ro>
2019-09-26 19:19:25 +0100

    displaying the grid

--- game/Cell.java
+++ game/Cell.java
@@ -1,14 +1,27 @@
 package game;
 
+import java.awt.Graphics;
+
 public class Cell {
 	private int x, y;
 	private int aliveNeighbours;
+	private Graphics g;
 	
-	public Cell() {
-		
+	public final static int size = 20;
+	
+	public Cell(Graphics g) {
+		this.g = g;
 	}
 	
 	public void countAliveNeighbours() {
+		
+	}
+
+	public void tick(Graphics g) {
+		
+	}
+	
+	public void render(Graphics g) {
 		
 	}
 	
--- game/Game.java
+++ game/Game.java
@@ -7,16 +7,18 @@ import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 	public static final int WIDTH = 800, HEIGHT = 650;
+
 	public Graphics g;
-	
+
 	private Thread thread;
+	private Handler handler;
 	private boolean running = false;
-	
+
 	public Game() {
 		new Window(WIDTH, HEIGHT, "Game of Life", this);
 	}
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		new Game();
 	}
 
@@ -37,7 +39,7 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	// THE GAME LOOP
-	@SuppressWarnings("unused") 
+	@SuppressWarnings("unused")
 	public void run() {
 		this.requestFocus();
 		long lastTime = System.nanoTime();
@@ -63,14 +65,14 @@ public class Game extends Canvas implements Runnable {
 //				System.out.println("FPS: " + frames);
 //				frames = 0;
 //			}
-			
-			//System.out.println(handler.bullets.size());
+
+			// System.out.println(handler.bullets.size());
 		}
 		stop();
 	}
 
 	private void tick() {
-		
+		// handler.tick();
 	}
 
 	private void render() {
@@ -85,11 +87,31 @@ public class Game extends Canvas implements Runnable {
 
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
-		//render code
+
+		drawGrid(g);
+
+		// handler.render();
 
 		g.dispose();
 		bs.show();
+	}
+
+	private void drawGrid(Graphics g) {
+								// golden
+		g.setColor(rgbToColor(218, 165, 32));
+
+		for (int k = 10; k < WIDTH; k += Cell.size) {
+			g.drawLine(k, 0, k, HEIGHT);
+			g.drawLine(0, k, WIDTH, k);
+		}
+	}
+
+	private Color rgbToColor(int r, int g, int b) {
+		float[] rgbValues = new float[3];
+		rgbValues = Color.RGBtoHSB(218, 165, 32, rgbValues);
+		Color c = (Color.getHSBColor(rgbValues[0], rgbValues[1], rgbValues[2]));
+
+		return c;
 	}
 
 	public static double clamp(double x, double min, double max) {
